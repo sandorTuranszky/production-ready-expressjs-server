9d750f8483faab8ee1bb37b05ed11a43897deed1
tsandor <tsandor@kwiff-mm-tsandor.local>
2019-01-15 12:13:12 +0100

    Rename kue folder to be agnostic of provider

--- src/utils/jobQueue/email.js
+++ src/utils/jobQueue/email.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const boom = require('boom');
 const { createTask } = require('./helpers');
 const { defaults } = require('./config');
 const { queue } = require('./helpers');
 const { send } = require('./integrations/mailjet');
+const winston = require('../logger/winston');
 
 const presetTemplates = {
   welcome: 'welcome-template',
@@ -17,10 +19,24 @@ const sendWelcomeEmail = (data, options) =>
     options,
   });
 
+const handleResult = (result, done) => {
+  winston.info(`Email sent: ${JSON.stringify(result)}`);
+  done();
+};
+
+const handleError = err => {
+  winston.error(`Error occurred during sending email: ${boom.boomify(err)}`);
+};
+
+/**
+ * Send email - MailJet implementation
+ * Any other email service can be used instead
+ */
 // eslint-disable-next-line no-unused-vars
 queue.process('email', defaults.email.concurrency, (job, done) => {
-  send(job.data);
-  done();
+  send(job.data)
+    .then(result => handleResult(result, done))
+    .catch(handleError);
 });
 
 module.exports = {
