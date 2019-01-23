ec1b73d1d7ec0d04fc6b6d54bcf930ad11dbcdf3
Sandor Turanszky <sandor.turanszky@gmail.com>
2019-01-23 13:42:54 +0100

    Add graphql-yoga server as a mid layer between the express app and prisma server

---
+++ src/graphql/resolvers/Mutation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Mutation = {
+  createUser(parent, args, { prisma }, info) {}, // eslint-disable-line
+  deleteUser(parent, args, { prisma }, info) {}, // eslint-disable-line
+  updateUser(parent, args, { prisma }, info) {}, // eslint-disable-line
+};
+
+module.export = Mutation;
