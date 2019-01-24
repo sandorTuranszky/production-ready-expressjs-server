3b5f3b3602f086bbb83da9b197584e55bf8380d9
Sandor Turanszky <sandor.turanszky@gmail.com>
2019-01-24 15:22:04 +0100

    Fix bug with empty mutations object

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,9 +1,32 @@
 'use strict';
 
 const Mutation = {
-  createUser(parent, args, { prisma }, info) {}, // eslint-disable-line
-  deleteUser(parent, args, { prisma }, info) {}, // eslint-disable-line
+  async createUser(parent, args, { prisma }, info) {
+    const emailTaken = await prisma.exists.User({ email: args.data.email });
+
+    if (emailTaken) {
+      throw new Error('Email taken');
+    }
+
+    return prisma.mutation.createUser({ data: args.data }, info);
+  },
+  async deleteUser(parent, args, { prisma }, info) {
+    const userExists = await prisma.exists.User({ id: args.id });
+
+    if (!userExists) {
+      throw new Error('User not found');
+    }
+
+    return prisma.mutation.deleteUser(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      info,
+    );
+  }, // eslint-disable-line
   updateUser(parent, args, { prisma }, info) {}, // eslint-disable-line
 };
 
-module.export = Mutation;
+module.exports = Mutation;
