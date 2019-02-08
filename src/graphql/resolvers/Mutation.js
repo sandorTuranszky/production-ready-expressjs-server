7655ccd69b8c3aa385c66b974015843492f96c21
Sandor Turanszky <sandor.turanszky@gmail.com>
2019-02-08 10:39:00 +0100

    Add registration and login with auth check

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -4,8 +4,10 @@ const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
+const errorMsg = 'Wrong email or password';
+
 const Mutation = {
-  async signUp(parent, args, { prisma }, info) { //eslint-disable-line
+  async createUser(parent, args, { prisma }, info) { //eslint-disable-line
     const password = await bcrypt.hash(args.data.password, 10);
     const user = await prisma.mutation.createUser({
       data: {
@@ -21,19 +23,24 @@ const Mutation = {
       user,
     };
   },
-  async login(parent, { data }, { prisma }, info) {
+  async login(parent, { data }, { prisma }, info) { //eslint-disable-line
     const { email, password } = data;
-    const user = await prisma.query.user({ where: { email } }, info);
-    if (!user) throw new Error('User not found');
+    const user = await prisma.query.user({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new Error(errorMsg);
 
     const valid = await bcrypt.compare(password, user.password);
-    if (!valid) throw new Error('Invalid password');
 
+    if (!valid) throw new Error(errorMsg);
     const token = jwt.sign({ userId: user.id }, config.get('app.secret'));
 
     return {
-      token,
       user,
+      token,
     };
   },
   async deleteUser(parent, args, { prisma }, info) {
